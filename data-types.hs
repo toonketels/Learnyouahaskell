c18f1a71d9696d96b5068c42189d07638a0ebb2c
Toon Ketels <toon@puresign.be>
2012-03-28 00:01:04 +0200

    Second version Shape - example data types.

--- data-types.hs
+++ data-types.hs
@@ -1,9 +1,16 @@
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0.0 0.0) r
+
+baseRectangle :: Float -> Float -> Shape
+baseRectangle width height = Rectangle (Point 0.0 0.0) (Point width height)
